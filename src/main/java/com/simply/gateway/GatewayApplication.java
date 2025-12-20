4c8ac33ec597749e0c8bdc3b461f818fefd00505
simplyhemant <hs2312hemant@gmail.com>
2025-12-20 15:42:59 +0530

    Initial commit - Gateway Service

---
+++ src/main/java/com/simply/gateway/GatewayApplication.java
@@ -0,0 +1,13 @@
+package com.simply.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/simply/gateway/GatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simply.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
